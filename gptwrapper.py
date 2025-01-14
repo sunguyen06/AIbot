b2552c370f91135db4f1a801bef8fdd88c43cc36
Su Nguyen <136272020+sunguyen06@users.noreply.github.com>
2025-01-13 20:59:01 -0500

    Updated wrapper message

--- gptwrapper.py
+++ gptwrapper.py
@@ -1,21 +1,21 @@
 from openai import OpenAI
 
 client = OpenAI(
-  api_key="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"
+  api_key="API_KEY_HERE"
 )
 
-def feedback(ca):
+def answer(prompt):
     completion = client.chat.completions.create(
         model="gpt-4o-mini",
         store=True,
         messages=[
-            {"role": "user", "content": f"According to Warren Buffet's idealogy on investments in the stock market. Analyze the following memecoin with this CA: {ca}."}
+            {"role": "user", "content": f{prompt}}
         ]
     )
 
     return (completion.choices[0].message)
 
 def main():
-    print(feedback("9aLx5SCcoacuK4VVmucy3yu7smR37TWXFyTHnxUQpump"))
+    print(answer("What's 3 + 6"))
 
 main()
